9e1c262a0a1d445727a3a9852b1bf01219fbba13
Gregory Norton <gregory.norton@me.com>
2019-08-18 15:22:35 -0400

    Added support for reading a range of addresses

--- eeprom_writer.py
+++ eeprom_writer.py
@@ -64,18 +64,23 @@ def write(address, fname):
             raise ProtocolException('Unexpected protocol message while awaiting response', end)
     return Protocol.END
 
-def read(address):
-    print address
+def read(address1, address2=None):
+    if (address2 == None):
+        address2 = address1 + 1
+    else:
+        address2 += 1
     
-    ser.write(chr(Protocol.READ.value))
-    ser.write(chr((address >> 8) & 0xFF))
-    ser.write(chr(address & 0xFF))
-    
-    out = ord(ser.read(size = 1))
-    result = ord(ser.read(size = 1))
-    if (result != Protocol.END.value):
-        print hex(result)
-        raise ProtocolException('Unexpected protocol message while awaiting response.', end)
+    out = []
+    for address in range(address1, address2):
+        ser.write(chr(Protocol.READ.value))
+        ser.write(chr((address >> 8) & 0xFF))
+        ser.write(chr(address & 0xFF))
+        out.append(ord(ser.read(size = 1)))
+        result = ord(ser.read(size = 1))
+        if (result != Protocol.END.value):
+            print hex(result)
+            raise ProtocolException('Unexpected protocol message while awaiting response.', end)
+            
     return out
     
 def dump(fname='dump.bin'):
@@ -105,10 +110,23 @@ def main(args):
             if uinput[0] == 'quit' or uinput[0] == 'q' or uinput[0] == 'exit':
                 active = False
             elif uinput[0] == 'read' or uinput[0] == 'r':
-                if len(uinput) != 2:
-                    print 'Invalid number of arguments'
+                if len(uinput) == 2:
+                    a1 = int(uinput[1], 0)
+                    out = read(a1)
+                    print '[' + hex(a1) + '] = ' + hex(out[0])
+                elif len(uinput) == 3:
+                    a1 = int(uinput[1], 0)
+                    a2 = int(uinput[2], 0)
+                    if a2 <= a1:
+                        print 'Second address must be greater than the first.'
+                        continue
+                    
+                    out = read(a1, a2)
+                    for i in range(0, a2 - a1 + 1):
+                        print '[' + hex(a1 + i) + '] = ' + hex(out[i])
                 else:
-                    print hex(read(int(uinput[1], 0)))
+                    print 'Invalid number of arguments'
+                    
             elif uinput[0] == 'write' or uinput[0] == 'w':
                 if (len(uinput) != 3):
                     print 'Invalid number of arguments'
@@ -126,7 +144,7 @@ def main(args):
                 dump()
             elif uinput[0] == 'help' or uinput[0] == 'h':
                 print 'Available commands'
-                print '\tread address1'# [address2]'
+                print '\tread address1 [address2]'
                 #print '\tpoke address [data]'
                 print '\twrite address file'
                 #print '\tlock'
